e163429a200f528097b98e6c4bff2c24d4c9f37b
Mohammad Anan <smoker_shuger@hotmail.com>
2023-07-12 16:19:07 +0300

    Add jwt authentication filter

---
+++ src/main/java/com/example/ordersystem/config/JwtService.java
@@ -0,0 +1,12 @@
+package com.example.ordersystem.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+
+    public String extractUserEmail(String token) {
+        return null;
+    }
+}
